ddd57e05b67a7d43e40ad82558f67ae2daaa620d
Lucas Carlos <lucascarls002@gmail.com>
2021-05-26 17:13:19 -0300

    Adicionar arquivos de projeto.

---
+++ TesteLucas/Models/Cartão.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteLucas.Models
+{
+    public class Cartão
+    {
+        public int Id { get; set; }
+
+        public string N_Cartao { get; set;}
+
+        public string Nome_Titular { get; set; }
+
+        public String Cpf_Titular { get; set; }
+
+        public int Cod_seguranca { get; set; }
+
+        public string Validade_Cartao { get; set; }
+
+        public int N_parcelas { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+
+
+
+    }
+}
---
+++ TesteLucas/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteLucas.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=Testelucas;Integrated Security= True");
+
+        }
+    }
+}
